6df15d2405afffabe8b77ea8e8efe9dc4d9bfb97
mohitpilla <mohitpilla@mohits-mbp>
2018-03-26 19:07:53 -0400

    cleaned and formatted program class

--- 1st/src/selenium/Program.java
+++ 1st/src/selenium/Program.java
@@ -8,10 +8,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Program {
-	
-	
-	public static void main(String[] args) throws InterruptedException
-	{
+
+	public static void main(String[] args) throws InterruptedException {
+		
 		String firstname = "automation";
 		String lastname = "test";
 		String mailid = "automationtest369";
@@ -23,22 +22,22 @@ public class Program {
 		String phonenumber;
 		String recoveryemail;
 		String countrycode;
-		
-			
-WebDriver driver=new SafariDriver();
-		
+
+		WebDriver driver = new SafariDriver();
+
 		driver.get("https://accounts.google.com");
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//*[text() = 'More options']")).click();
 		Thread.sleep(3000);
-		
+
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 
-		WebElement option = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"initialView\"]/div[2]/div[3]/div/div/content[1]")));
+		WebElement option = wait.until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//*[@id=\"initialView\"]/div[2]/div[3]/div/div/content[1]")));
 		option.click();
 		option.clear();
 		Thread.sleep(5000);
-		
+
 		driver.findElement(By.name("FirstName")).sendKeys(firstname);
 		driver.findElement(By.id("LastName")).sendKeys(lastname);
 		driver.findElement(By.name("GmailAddress")).sendKeys(mailid);
@@ -49,32 +48,24 @@ WebDriver driver=new SafariDriver();
 		driver.findElement(By.id("BirthYear")).sendKeys("1991");
 		driver.findElement(By.xpath("//*[@id=\"Gender\"]/div")).sendKeys("rather not say");
 		Thread.sleep(3000);
-		//driver.manage().window().maximize();
 		driver.findElement(By.xpath("//*[@id=\"RecoveryPhoneNumber\"]")).sendKeys("3042828281");
 		driver.findElement(By.name("RecoveryEmailAddress")).sendKeys("pllmht8@gmail.com");
 		driver.findElement(By.xpath("//*[@id=\"CountryCode\"]/div[1]")).sendKeys("india");
 		Thread.sleep(3000);
 		driver.findElement(By.name("submitbutton")).click();
 		Thread.sleep(3000);
-//		driver.manage().window().maximize();
-		for (int i = 0; i < 50; i++) 
-		{
-		driver.findElement(By.xpath("//*[@id=\"tos-scroll-button\"]")).click();
+		for (int i = 0; i < 50; i++) {
+			driver.findElement(By.xpath("//*[@id=\"tos-scroll-button\"]")).click();
 		}
 		Thread.sleep(3000);
-		if(!driver.findElement(By.name("iagreebutton")).isDisplayed());
+		if (!driver.findElement(By.name("iagreebutton")).isDisplayed())
+			;
 		{
 			driver.findElement(By.name("iagreebutton")).click();
 		}
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//*[@id=\"signupidvmethod-voice\"]")).click();
-//			{
-//			System.out.print("FILE UPLOAD SUCCESSFUL");
-//			}
-//			}
-}
 
-	
-		
 	}
 
+}
